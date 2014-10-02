9644652390fbdaf5b67bf8e9be671761c0a55e1b
“Jen <mpjeno@gmail.com>
2014-10-01 21:25:33 -0500

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelayedJobPractice::Application.config.secret_token = 'be66afa1c5957174581f5b1e77cef70edd012f0363d95cf626fdd015e233e4388dc31f8bfe3738bac34dc58b5ebc91f169acce24d9b2a0f266b6acd03a7119eb'
